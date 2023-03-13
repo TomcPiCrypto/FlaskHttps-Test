614426774e040e9ba55e81198a72c523259bb281
Tom Cohen <99245711+TomcPiCrypto@users.noreply.github.com>
2023-03-13 17:52:26 +0200

    https flask test

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello from Python microservice!"
+
+if __name__ == '__main__':
+    context = ('cert.pem', 'key.pem') # SSL/TLS certificates and keys
+    app.run(host='0.0.0.0', port=9977, ssl_context=context)
